2eaa51e0bb2405e00a3036d20467259e7d858f81
Alex <alex404an@gmail.com>
2023-06-01 14:35:21 -0400

    Update UserDao.java
    Added a method that searches the database by username

--- Spring-Boot/src/main/java/com/genspark/SpringBootdemoApplication/Dao/UserDao.java
+++ Spring-Boot/src/main/java/com/genspark/SpringBootdemoApplication/Dao/UserDao.java
@@ -6,4 +6,12 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserDao extends JpaRepository<Users, Integer> {
+
+
+  /**
+   * For the findBy method to work:
+   *  - the method has to start exactly with findBy. the B has to be capitalized
+   *  - the word after the findBy has to be a column name. It is case-sensitive, but the first letter can be upper case since it will turn to lower case
+   */
+  Users findByUsername(String u);
 }
