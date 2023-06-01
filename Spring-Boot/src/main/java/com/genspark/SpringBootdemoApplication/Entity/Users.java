76e8dff075ef89c8daa2f4ad4aae4057a82862a2
Alex <alex404an@gmail.com>
2023-06-01 14:34:56 -0400

    Update User File
    Made an update to the users file to add getters, setters, and constructors.

--- Spring-Boot/src/main/java/com/genspark/SpringBootdemoApplication/Entity/Users.java
+++ Spring-Boot/src/main/java/com/genspark/SpringBootdemoApplication/Entity/Users.java
@@ -13,5 +13,44 @@ public class Users {
   private String password;
   private String role;
 
+  public Users() {
 
+  }
+  public Users(String username, String password, String role) {
+    this.username = username;
+    this.password = password;
+    this.role = role;
+  }
+
+  public int getUser_id() {
+    return user_id;
+  }
+
+  public void setUser_id(int user_id) {
+    this.user_id = user_id;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public String getRole() {
+    return role;
+  }
+
+  public void setRole(String role) {
+    this.role = role;
+  }
 }
